897a09062f5b4241461912d60e9db7f411b7fb00
Harum Helmy <harumendhah@gmail.com>
2019-08-12 08:46:21 -0400

    Done.

--- app/controllers/sightings_controller.rb
+++ app/controllers/sightings_controller.rb
@@ -1,6 +1,11 @@
 class SightingsController < ApplicationController
-    def show
-        @sighting = Sighting.find(params[:id])
+    def index
+        sightings = Sighting.all
         render json: SightingSerializer.new(sightings).to_serialized_json
+    end
+
+    def show
+        sighting = Sighting.find(params[:id])
+        render json: SightingSerializer.new(sighting).to_serialized_json
     end
 end
